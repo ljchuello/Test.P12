f60c3a6a5bfc9d532f33d147ab01ed98e3e06870
Leonardo Chuello <ljchuello@gmail.com>
2019-07-25 09:47:55 -0500

    Creado metro framework

--- Test.P12/Form1.cs
+++ Test.P12/Form1.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using MetroFramework.Forms;
 
 namespace Test.P12
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MetroForm
     {
         public Form1()
         {
             InitializeComponent();
+            Text = "Validación firma eleclectrónica (.P12)";
         }
     }
 }
