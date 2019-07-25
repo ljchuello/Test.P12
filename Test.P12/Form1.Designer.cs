0a24f40ad450df124bd5e156f06449d8568d33a5
Leonardo Chuello <ljchuello@gmail.com>
2019-07-25 10:11:45 -0500

    Comprobación de la firma electrónica con éxito

--- Test.P12/Form1.Designer.cs
+++ Test.P12/Form1.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblFechaEmision = new MetroFramework.Controls.MetroLabel();
+            this.lblFechaVencimiento = new MetroFramework.Controls.MetroLabel();
+            this.btnValidar = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(292, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.TextBox1_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(24, 64);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(201, 19);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "Ubicación de la firma electrónica";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(24, 109);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(143, 19);
+            this.metroLabel2.TabIndex = 3;
+            this.metroLabel2.Text = "Contraseña de la firma";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(24, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(292, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // lblFechaEmision
+            // 
+            this.lblFechaEmision.AutoSize = true;
+            this.lblFechaEmision.Location = new System.Drawing.Point(23, 154);
+            this.lblFechaEmision.Name = "lblFechaEmision";
+            this.lblFechaEmision.Size = new System.Drawing.Size(114, 19);
+            this.lblFechaEmision.TabIndex = 4;
+            this.lblFechaEmision.Text = "Fecha de emisión:";
+            // 
+            // lblFechaVencimiento
+            // 
+            this.lblFechaVencimiento.AutoSize = true;
+            this.lblFechaVencimiento.Location = new System.Drawing.Point(24, 173);
+            this.lblFechaVencimiento.Name = "lblFechaVencimiento";
+            this.lblFechaVencimiento.Size = new System.Drawing.Size(139, 19);
+            this.lblFechaVencimiento.TabIndex = 5;
+            this.lblFechaVencimiento.Text = "Fecha de vencimiento:";
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(24, 196);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(292, 23);
+            this.btnValidar.TabIndex = 6;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseSelectable = true;
+            this.btnValidar.Click += new System.EventHandler(this.BtnValidar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(549, 446);
+            this.ClientSize = new System.Drawing.Size(342, 246);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.lblFechaVencimiento);
+            this.Controls.Add(this.lblFechaEmision);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private MetroFramework.Controls.MetroLabel lblFechaEmision;
+        private MetroFramework.Controls.MetroLabel lblFechaVencimiento;
+        private MetroFramework.Controls.MetroButton btnValidar;
     }
 }
 
--- Test.P12/Form1.cs
+++ Test.P12/Form1.cs
@@ -1,4 +1,8 @@
-﻿using MetroFramework.Forms;
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using MetroFramework.Forms;
 
 namespace Test.P12
 {
@@ -8,6 +12,52 @@ namespace Test.P12
         {
             InitializeComponent();
             Text = "Validación firma eleclectrónica (.P12)";
+        }
+
+        private void TextBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void BtnValidar_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                // Validamos que exista el archivo
+                if (!File.Exists(textBox1.Text))
+                {
+                    MessageBox.Show($"No se encuntra la firma electrónica");
+                    return;
+                }
+
+                // Validamos que el archivo sea .P12
+                FileInfo fileInfo = new FileInfo(textBox1.Text);
+                if (fileInfo.Extension.ToLower() != ".p12")
+                {
+                    MessageBox.Show($"La extensión de la firma electrónica debe ser .P12");
+                    return;
+                }
+
+                // Validamos la firma
+                X509Certificate2 x509Certificate2 = new X509Certificate2(textBox1.Text, textBox2.Text, X509KeyStorageFlags.DefaultKeySet);
+
+                // Colocamos las fechas
+                lblFechaEmision.Text = $"Fecha de emisión: {x509Certificate2.NotBefore:dd/MM/yyyy}";
+                lblFechaVencimiento.Text = $"Fecha de vencimiento: {x509Certificate2.NotAfter:dd/MM/yyyy}";
+
+                MessageBox.Show("Firma electrónica correcta");
+            }
+            catch (Exception ex)
+            {
+                // Colocamos las fechas
+                lblFechaEmision.Text = $"Fecha de emisión:";
+                lblFechaVencimiento.Text = $"Fecha de vencimiento:";
+
+                MessageBox.Show($"Ah ocurido un error; {ex.Message}");
+            }
         }
     }
 }
